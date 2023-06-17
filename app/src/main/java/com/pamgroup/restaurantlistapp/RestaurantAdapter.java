e4e226e14f884f85dc969903a6a60861f6c55342
Irfan Hanif <14.irfanh@gmail.com>
2023-06-17 16:23:36 +0700

    Updated UI for restaurant_item_layout.xml and RestaurantAdapter class

--- app/src/main/java/com/pamgroup/restaurantlistapp/RestaurantAdapter.java
+++ app/src/main/java/com/pamgroup/restaurantlistapp/RestaurantAdapter.java
@@ -1,33 +1,78 @@
 package com.pamgroup.restaurantlistapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RestaurantAdapter extends RecyclerView.Adapter {
+import com.pamgroup.restaurantlistapp.model.Restaurant;
+
+import java.util.List;
+
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Restaurant> restaurantList;
+
+    public RestaurantAdapter(Context context) {
+        this.context = context;
+    }
+
+    public List<Restaurant> getRestaurantList() {
+        return restaurantList;
+    }
+
+    public void setRestaurantList(List<Restaurant> restaurantList) {
+        this.restaurantList = restaurantList;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurant_item_layout, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Restaurant restaurant = restaurantList.get(position);
 
+        holder.tvName.setText(restaurant.getName());
+        holder.tvAddress.setText(restaurant.getAddress());
+
+        holder.acivEdit.setOnClickListener(view -> {
+
+        });
+
+        holder.acivDelete.setOnClickListener(view -> {
+
+        });
     }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return restaurantList.size();
     }
 
-    private class ViewHolder extends RecyclerView.ViewHolder{
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvName, tvAddress;
+        private AppCompatImageView acivEdit, acivDelete;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            tvName = itemView.findViewById(R.id.tv_name);
+            tvAddress = itemView.findViewById(R.id.tv_address);
+            acivEdit = itemView.findViewById(R.id.aciv_edit);
+            acivDelete = itemView.findViewById(R.id.aciv_delete);
         }
     }
 }
