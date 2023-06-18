f08d6c6a0b4618b88d383f5247259ee510fb361d
Irfan Hanif <14.irfanh@gmail.com>
2023-06-18 20:04:49 +0700

    Updated to choose Image

--- app/src/main/java/com/pamgroup/restaurantlistapp/helper/ImageStorage.java
+++ app/src/main/java/com/pamgroup/restaurantlistapp/helper/ImageStorage.java
@@ -3,12 +3,7 @@ package com.pamgroup.restaurantlistapp.helper;
 import android.net.Uri;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.TaskCompletionSource;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -27,23 +22,22 @@ public class ImageStorage {
         storageRef = storage.getReference();
     }
 
-    public String uploadImage(String filePath) {
+    public Task<Uri> uploadImage(String filePath) {
         file = Uri.fromFile(new File(filePath));
         imageRef = storageRef.child("images/" + file.getLastPathSegment());
         UploadTask uploadTask = imageRef.putFile(file);
 
-        uploadTask.addOnFailureListener(exception -> {
+        return uploadTask.continueWithTask(task -> {
+            if (!task.isSuccessful()) {
+                throw task.getException();
+            }
+            return imageRef.getDownloadUrl();
+        }).addOnSuccessListener(uri -> {
+            imageURL = uri.toString();
+            Log.d("IMAGEURL", imageURL);
+        }).addOnFailureListener(exception -> {
             Log.e("ERROR-UPLOAD", exception.getMessage());
-        }).addOnSuccessListener(taskSnapshot -> {
-            imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
-                imageURL = uri.toString();
-                Log.d("IMAGEURL", imageURL);
-            }).addOnFailureListener(exception -> {
-                Log.e("ERROR-GET-IMAGE-URL", exception.getMessage());
-            });
         });
-
-        return imageURL;
     }
 
 
