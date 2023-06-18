cac582a16dce8971dee8646d74a974ee27a392c6
Irfan Hanif <14.irfanh@gmail.com>
2023-06-18 10:02:18 +0700

    finished uploadImage() method

--- app/src/main/java/com/pamgroup/restaurantlistapp/helper/ImageStorage.java
+++ app/src/main/java/com/pamgroup/restaurantlistapp/helper/ImageStorage.java
@@ -1,6 +1,7 @@
 package com.pamgroup.restaurantlistapp.helper;
 
 import android.net.Uri;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
@@ -19,58 +20,31 @@ public class ImageStorage {
     private FirebaseStorage storage;
     private StorageReference storageRef, imageRef;
     private Uri file;
+    private String imageURL;
 
-    public ImageStorage(){
+    public ImageStorage() {
         storage = FirebaseStorage.getInstance();
         storageRef = storage.getReference();
     }
 
     public String uploadImage(String filePath) {
-        String imageURL = "";
-        uploadImageTask(filePath).addOnSuccessListener(new OnSuccessListener<String>() {
-            @Override
-            public void onSuccess(String URL) {
-                // Handle successful image upload
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Handle unsuccessful image upload
-            }
-        });
-    }
-
-    public Task<String> uploadImageTask(String filePath) {
-        TaskCompletionSource<String> taskCompletionSource = new TaskCompletionSource<>();
-
         file = Uri.fromFile(new File(filePath));
         imageRef = storageRef.child("images/" + file.getLastPathSegment());
         UploadTask uploadTask = imageRef.putFile(file);
 
-        uploadTask.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                taskCompletionSource.setException(exception);
-            }
-        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        String imageURL = uri.toString();
-                        taskCompletionSource.setResult(imageURL);
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        taskCompletionSource.setException(e);
-                    }
-                });
-            }
+        uploadTask.addOnFailureListener(exception -> {
+            Log.e("ERROR-UPLOAD", exception.getMessage());
+        }).addOnSuccessListener(taskSnapshot -> {
+            imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                imageURL = uri.toString();
+                Log.d("IMAGEURL", imageURL);
+            }).addOnFailureListener(exception -> {
+                Log.e("ERROR-GET-IMAGE-URL", exception.getMessage());
+            });
         });
 
-        return taskCompletionSource.getTask();
+        return imageURL;
     }
+
 
 }
