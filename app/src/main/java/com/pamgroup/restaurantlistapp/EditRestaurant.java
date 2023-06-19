a78b8077e0dfb62db2d369677a9e1b0b8861180a
Irfan Hanif <14.irfanh@gmail.com>
2023-06-19 16:40:11 +0700

    Update Edit restaurant layout and logic. Fixed error of project build

--- app/src/main/java/com/pamgroup/restaurantlistapp/EditRestaurant.java
+++ app/src/main/java/com/pamgroup/restaurantlistapp/EditRestaurant.java
@@ -1,19 +1,30 @@
 package com.pamgroup.restaurantlistapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.pamgroup.restaurantlistapp.helper.ImageStorage;
+import com.pamgroup.restaurantlistapp.helper.RestaurantDatabase;
 
-public class EditRestaurant extends AppCompatActivity {
+public class EditRestaurant extends AppCompatActivity implements View.OnClickListener {
 
+    private ImageStorage imageStorage;
+    private RestaurantDatabase restaurantDatabase;
     private EditText etName, etAddress, etBusinessHour, etDescription;
-    private Button btnSave;
+    private Button btnCreate, btnHapus;
+    private ImageView btnBack, btnChooseImage;
+    private static final int PICK_IMAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +35,10 @@ public class EditRestaurant extends AppCompatActivity {
         etAddress = findViewById(R.id.etAlamat);
         etBusinessHour = findViewById(R.id.etJamBukaTutup);
         etDescription = findViewById(R.id.etDeskripsi);
-        btnSave = findViewById(R.id.btnCreate);
+        btnBack = findViewById(R.id.btn_back);
+        btnHapus = findViewById(R.id.btn_hapus);
+        btnCreate = findViewById(R.id.btnCreate);
+        btnChooseImage = findViewById(R.id.upload_image);
 
         Bundle restaurantBundle = getIntent().getBundleExtra("restaurantBundle");
         if (restaurantBundle != null) {
@@ -39,32 +53,96 @@ public class EditRestaurant extends AppCompatActivity {
             etDescription.setText(description);
         }
 
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Save the updated restaurant details
-                String updatedName = etName.getText().toString();
-                String updatedAddress = etAddress.getText().toString();
-                String updatedBusinessHour = etBusinessHour.getText().toString();
-                String updatedDescription = etDescription.getText().toString();
+        btnCreate.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+        btnHapus.setOnClickListener(this);
+        btnChooseImage.setOnClickListener(this);
+    }
 
-                Bundle restaurantBundle = getIntent().getBundleExtra("restaurantBundle");
-                if (restaurantBundle != null) {
-                    String restaurantId = restaurantBundle.getString("restaurantId");
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnCreate:
+                String name = etName.getText().toString();
+                String address = etAddress.getText().toString();
+                String businessHour = etBusinessHour.getText().toString();
+                String description = etDescription.getText().toString();
 
-                    // Get a reference to the restaurant node in the database
-                    DatabaseReference restaurantRef = FirebaseDatabase.getInstance().getReference().child("restaurants").child(restaurantId);
+                if (!validateForm(name, address, businessHour, description))
+                    return;
 
-                    // Update the restaurant details
-                    restaurantRef.child("name").setValue(updatedName);
-                    restaurantRef.child("address").setValue(updatedAddress);
-                    restaurantRef.child("businessHour").setValue(updatedBusinessHour);
-                    restaurantRef.child("description").setValue(updatedDescription);
+                Thread thread = new Thread(() -> {
+                    String imageURL = imageStorage.getImageURL();
+                    RestaurantDatabase database = new RestaurantDatabase();
+                    database.addRestaurant(name, address, businessHour, description, imageURL);
+                });
+                thread.start();
+                finish();
+                break;
 
-                    // Finish the activity and return to the previous screen
-                    finish();
-                }
-            }
+            case R.id.upload_image:
+                selectImage();
+                break;
+
+            case R.id.btn_back:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_hapus:
+                etName.setText("");
+                etAddress.setText("");
+                etBusinessHour.setText("");
+                etDescription.setText("");
+                break;
+        }
+    }
+
+    private boolean validateForm(String name, String address, String businessHour, String description) {
+        if (TextUtils.isEmpty(name)) {
+            etName.setError("Masukkan nama restoran");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(address)) {
+            etAddress.setError("Masukkan alamat restoran");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(businessHour)) {
+            etBusinessHour.setError("Masukkan jam buka-tutup restoran");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(description)) {
+            etDescription.setError("Masukkan deskripsi restoran");
+            return false;
+        }
+
+
+        return true;
+    }
+
+    private void selectImage() {
+        Thread thread = new Thread(() -> {
+            Intent imageIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            imageIntent.setType("image/*");
+            imageIntent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(imageIntent, "Select Image"), PICK_IMAGE_REQUEST);
         });
+        thread.start();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri selectedImageUri = data.getData();
+            Thread thread = new Thread(() -> {
+                if (selectedImageUri != null) {
+                    imageStorage.uploadImage(selectedImageUri);
+                }
+            });
+            thread.start();
+            runOnUiThread(() -> btnChooseImage.setImageURI(selectedImageUri));
+        }
     }
 }
