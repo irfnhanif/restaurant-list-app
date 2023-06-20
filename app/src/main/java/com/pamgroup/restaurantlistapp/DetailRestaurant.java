ebbdb1caf3f820f5c8551d4d906cf9e83efde16e
isntchar <112851018+isntchar@users.noreply.github.com>
2023-06-20 09:35:38 +0700

    download image

--- app/src/main/java/com/pamgroup/restaurantlistapp/DetailRestaurant.java
+++ app/src/main/java/com/pamgroup/restaurantlistapp/DetailRestaurant.java
@@ -1,21 +1,33 @@
 package com.pamgroup.restaurantlistapp;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
 
 public class DetailRestaurant extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tvName, tvAddress, tvBusinessHour, tvDescription;
     private ImageView ivRestaurant, btn_back;
+    private String restaurantName, imgUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +45,17 @@ public class DetailRestaurant extends AppCompatActivity implements View.OnClickL
 
         Bundle restaurantBundle = getIntent().getBundleExtra("restaurantBundle");
         if (restaurantBundle != null) {
-            String name = restaurantBundle.getString("name");
+            restaurantName = restaurantBundle.getString("name");
             String address = restaurantBundle.getString("address");
             String businessHour = restaurantBundle.getString("businessHour");
             String description = restaurantBundle.getString("description");
-            String imageURL = restaurantBundle.getString("imageURL");
+            imgUrl = restaurantBundle.getString("imageURL");
 
-            tvName.setText(name);
+            if (imgUrl != null) {
+                downloadImage();
+            }
+
+            tvName.setText(restaurantName);
             tvAddress.setText(address);
             tvBusinessHour.setText(businessHour);
             tvDescription.setText(description);
@@ -50,9 +66,56 @@ public class DetailRestaurant extends AppCompatActivity implements View.OnClickL
                     .error(R.drawable.ic_error);
 
             Glide.with(this)
-                    .load(imageURL)
+                    .load(imgUrl)
                     .apply(requestOptions)
                     .into(ivRestaurant);
+        }
+    }
+   // Download image
+    private void downloadImage() {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        try {
+            // Create storage reference
+            StorageReference storageRef = storage.getReferenceFromUrl(imgUrl);
+
+            File localFile;
+            File storagePath = new File(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "");
+
+            if (!storagePath.exists()) {
+                storagePath.mkdirs();
+            }
+
+            if(storagePath.exists()) {
+                localFile = storagePath;
+            } else {
+                localFile = getApplication().getExternalFilesDir("images");
+            }
+
+            final File myFile = new File(localFile, restaurantName + ".jpg");
+
+            storageRef.getFile(myFile).addOnSuccessListener(taskSnapshot -> {
+                // Local file berhasil created
+                Toast.makeText(this, "File downloaded", LENGTH_SHORT).show();
+            }).addOnFailureListener(exception -> {
+                // Handle errors
+                Toast.makeText(getBaseContext(), "Download failed. Try again!", LENGTH_SHORT).show();
+                exception.printStackTrace();
+            });
+
+
+            new Handler().postDelayed(() -> {
+                ContentValues values = new ContentValues();
+
+                values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
+                values.put(MediaStore.Images.Media.DISPLAY_NAME, restaurantName);
+                values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                values.put(MediaStore.MediaColumns.DATA, myFile.getAbsolutePath());
+                getApplicationContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            }, 1000);
+            ContentValues values = new ContentValues();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
