e5cfc77eadab21fefd9d32cb97d58036662c2556
Irfan Hanif <14.irfanh@gmail.com>
2023-06-21 20:20:34 +0700

    Created Emoji and API to get emoji from URL

---
+++ app/src/main/java/com/pamgroup/restaurantlistapp/API.java
@@ -0,0 +1,68 @@
+package com.pamgroup.restaurantlistapp;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.pamgroup.restaurantlistapp.model.Emoji;
+
+import org.json.JSONException;
+
+import java.util.List;
+
+public class API {
+    private static API instance = null;
+    private static final String URL = "https://emojihub.yurace.pro/api/random/category/food-and-drink";
+    public static RequestQueue requestQueue;
+
+    private API(Context context) {
+        requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+    }
+
+    public static synchronized API getInstance(Context context) {
+        if (null == instance) instance = new API(context);
+        return instance;
+    }
+
+    public static synchronized API getInstance() {
+        if (null == instance)
+            throw new IllegalStateException(API.class.getSimpleName() + "belum diinisialisasi");
+        return instance;
+    }
+
+    public interface DataCallback {
+        void onDataReceived(Emoji emoji);
+
+        void onError(Exception e);
+    }
+
+
+    public Emoji getEmoji(final DataCallback callback) {
+        final Emoji emoji = new Emoji();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL, null,
+                response -> {
+                    try {
+                        String name = response.getString("name");
+                        String unicode = response.getJSONArray("unicode").getString(0);
+
+                        emoji.setName(name);
+                        emoji.setUniCode(unicode);
+
+                        callback.onDataReceived(emoji);
+                    } catch (JSONException e) {
+                        callback.onError(e);
+                    }
+                },
+                error -> {
+                    callback.onError(new Exception(error.getMessage()));
+                });
+
+        requestQueue.add(jsonObjectRequest);
+
+        return emoji;
+    }
+
+}
---
+++ app/src/main/java/com/pamgroup/restaurantlistapp/model/Emoji.java
@@ -0,0 +1,23 @@
+package com.pamgroup.restaurantlistapp.model;
+
+public class Emoji {
+    private String name;
+    private String uniCode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUniCode() {
+        return uniCode;
+    }
+
+    public void setUniCode(String uniCode) {
+        this.uniCode = uniCode;
+    }
+
+}
